731162bb4cc3b73fb48a44bd5b7c6523954689ed
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-07-01 13:32:51 +0300

    Change error messages

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -45,19 +45,19 @@ class TestIntegration(unittest.TestCase):
         self.assertIn("classes", response)
 
     def test_api_response_with_too_long_text(self):
-        # TODO FAIL Internal error
-        long_text = "Long set. " * 200
+        # TODO FAIL Internal error with value 200
+        long_text = "Long set. " * 300
         payload = create_payload(long_text)
         response = call_api(payload)
         self.assertEqual(response['failure']['errors'][0]['code'],
-                         'elg.request.too.large')
+                         'lingsoft.token.too.many')
 
     def test_api_response_with_long_token(self):
-        # TODO Failure (give own error message)
-        long_token = "å" * 2000
+        long_token = "å" * 101
         payload = create_payload(long_token)
         response = call_api(payload)
-        self.assertIn("classes", response)
+        self.assertEqual(response['failure']['errors'][0]['code'],
+                         'lingsoft.token.too.long')
 
     def test_api_response_with_special_characters(self):
         spec_text = "\N{grinning face}\u4e01\u0009" + self.steady_text + "\u0008"
